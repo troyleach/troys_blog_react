314c94260401c18ae7d1925857b0fe649a391b7e
Troy Leach <troyleach29@gmail.com>
2020-03-19 20:43:23 -0500

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,21 +1,22 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "NWE5ODliYjFlMmRiNzRmNzYwNWE5MWUyNTEzYWE1NTM1MWM0N2M2NQ==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "troyleach",
 
   // Your Github Repo Name Where You Have your issues as Blog
   githubRepo: "react-blog-github",
 
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
-  enableOAuth: true,
+  // enableOAuth: true,
+  enableOAuth: false,
   OAuthClientID: 'Iv1.9f40fd53257d4102',
   OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "Troy's Blog" ,
 
   // Your Personal Blog Subtitle
   subtitle : "Software Developer",
