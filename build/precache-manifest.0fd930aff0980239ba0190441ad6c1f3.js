c203adcc1d3e486daf95a703d19cdda45c94e31a
Troy Leach <troyleach29@gmail.com>
2020-03-28 16:17:46 -0500

    url changes

--- build/precache-manifest.0fd930aff0980239ba0190441ad6c1f3.js
+++ build/precache-manifest.0fd930aff0980239ba0190441ad6c1f3.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e8139ef032db37b2a151d66890f8bcd5",
-    "url": "/troys_blog_react/index.html"
+    "url": "/troyleach/troys_blog_react/index.html"
   },
   {
     "revision": "19d09982548ed2760926",
-    "url": "/troys_blog_react/static/css/main.85cb079d.chunk.css"
+    "url": "/troyleach/troys_blog_react/static/css/main.85cb079d.chunk.css"
   },
   {
     "revision": "4d209e43a3d6ded32226",
-    "url": "/troys_blog_react/static/js/2.719ef627.chunk.js"
+    "url": "/troyleach/troys_blog_react/static/js/2.719ef627.chunk.js"
   },
   {
     "revision": "189e23ef3562d267680aa9086e84695d",
-    "url": "/troys_blog_react/static/js/2.719ef627.chunk.js.LICENSE"
+    "url": "/troyleach/troys_blog_react/static/js/2.719ef627.chunk.js.LICENSE"
   },
   {
     "revision": "19d09982548ed2760926",
-    "url": "/troys_blog_react/static/js/main.dca8d8c7.chunk.js"
+    "url": "/troyleach/troys_blog_react/static/js/main.dca8d8c7.chunk.js"
   },
   {
     "revision": "1a5c8f85ecc34b30ee07",
-    "url": "/troys_blog_react/static/js/runtime-main.ef76bb1e.js"
+    "url": "/troyleach/troys_blog_react/static/js/runtime-main.ef76bb1e.js"
   },
   {
     "revision": "65f8709df0f0bfa7d0a05d7f9517ba07",
-    "url": "/troys_blog_react/static/media/loader.65f8709d.svg"
+    "url": "/troyleach/troys_blog_react/static/media/loader.65f8709d.svg"
   }
 ]);
